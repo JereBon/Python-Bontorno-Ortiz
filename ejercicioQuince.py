1c934a888e337a1189d866effe47143c06214bb3
JereBon <jeremiasbontorno5@gmail.com>
2024-08-30 12:31:32 -0300

    arreglado multiplo de 3, 6 y 9

--- ejercicioQuince.py
+++ ejercicioQuince.py
@@ -1,15 +1,23 @@
+def suma_cifras(x):
+    suma = 0
+    while x > 0:
+        suma += x % 10
+        x //= 10
+    return suma
+
 x = 0
 x = int(input("Ingrese un número entero: "))
 print("El número ", x ," es divisible por:")
+
 if x % 2 == 0:
     print("2")
-if ((x // 10) + (x % 10)) % 3 == 0:
+if suma_cifras(x) % 3 == 0:
     print("3")
 if x % 10 == 0 or x % 10 == 5:
     print("5")
-if x % 2 == 0 and ((x // 10) + (x % 10)) % 3 == 0:
+if x % 2 == 0 and suma_cifras(x) % 3 == 0:
     print("6")
-if ((x // 10) + (x % 10)) % 9 == 0:
+if suma_cifras(x) % 9 == 0:
     print("9")
 if x % 10 == 0:
     print("10")
